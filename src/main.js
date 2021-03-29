ba77c0e67799e7c12cecfc6619d5fda53081ca6b
enoch454 <aegc.45@gmail.com>
2021-03-28 22:40:17 -0600

    Separado de la configuracion de router

--- src/main.js
+++ src/main.js
@@ -1,29 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-import {createRouter, createWebHistory} from 'vue-router'
-
-const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: PageHome
-    },
-    {
-        path: '/thread/:id',
-        name: 'ThreadShow',
-        component: PageThreadShow,
-        props: true,
-    },
-]
-
-const router = createRouter(
-    {
-        history: createWebHistory(),
-        routes, // short for `routes: routes`
-    })
-  
+import router from '@/router'  
 
 const app_masterclass = createApp(App)
 app_masterclass.use(router)
