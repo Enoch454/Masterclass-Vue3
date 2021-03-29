d9d5d7f10ef0c95121d46d13b630ceb5d159557a
enoch454 <aegc.45@gmail.com>
2021-03-28 23:55:54 -0600

    creacion de componente thread list y limpieza home

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,58 +1,22 @@
 <template>
-    <div class="col-large push-top"
-        v-for="item in threads"
-        :key="item.id">
-      
-        <h2>{{item.title}}</h2>
-
-        <div class="post-list">
-            <div
-                class="post"
-                v-for="postId in item.posts"
-                :key="postId"
-                >
-                <div class="user-info">
-                    <a href="#" class="user-name">
-                        {{ userById(postById(postId).userId).name }}
-                    </a>
-                    <a href="#">
-                        <img :src="userById(postById(postId).userId).avatar" alt="" class="avatar-large">
-                    </a>
-                </div>
-                <div class="post-content">
-                    <div>
-                        <p>
-                            {{ postById(postId).text }}
-                        </p>
-                    </div>
-                </div>
-                <div class="post-date text-faded">
-                    {{postById(postId).publishedAt}}
-                </div>
-            </div>
-        </div>  
-        
-  </div>
+    <h1>Wellcome to the forum</h1>
+    <ThreadList :threads = "threads"/>
 </template>
 
 <script>
-import sourceData from '@/data.json';
-
+import ThreadList from '@/components/ThreadList'
+import sourceData from '@/data.json'
 export default {
+    components: {
+        ThreadList, 
+    },
     data() {
         return {
             threads: sourceData.threads,
-            posts: sourceData.posts,
-            users: sourceData.users,
         }
     },
     methods:{
-        postById(postId){
-            return this.posts.find(x => x.id === postId)
-        },
-        userById(userId){
-            return this.users.find(x => x.id === userId)
-        }
+        
     }
 }
 </script>
